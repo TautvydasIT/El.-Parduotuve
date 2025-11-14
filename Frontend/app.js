1f8f0dbfd964badcde4b4b9c3bd11a3c59e2e289
TautvydasIT <tautvisjomano@gmail.com>
2025-11-14 19:57:45 +0200

    d

--- Frontend/app.js
+++
@@ -1,8 +0,0 @@
-const { createElement } = React;
-const { render } = ReactDOM;
-
-function App() {
-  return createElement('h1', { style: { textAlign: 'center', marginTop: '50px' } }, 'Elektroninė parduotuvė');
-}
-
-render(createElement(App), document.getElementById('root'));
