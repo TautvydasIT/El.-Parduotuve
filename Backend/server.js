d5f026b18b7964a00c155f858e320e408eac896d
TautvydasIT <tautvisjomano@gmail.com>
2025-11-17 01:01:32 +0200

    yes

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import typeRoutes from "./routes/typeRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -12,6 +13,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/types", typeRoutes);
