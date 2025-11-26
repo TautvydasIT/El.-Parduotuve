63371596dfc90581e043b0b7d1d0f284302eca39
TautvydasIT <tautvisjomano@gmail.com>
2025-11-26 16:03:38 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./pages/About";
+import Help from "./pages/Help";
 
 export default function App() {
   return (
