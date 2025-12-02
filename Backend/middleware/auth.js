3dc43d6c4f14807e66447ed102207a9dc6b0e88f
TautvydasIT <tautvisjomano@gmail.com>
2025-12-02 03:48:05 +0200

    Update auth.js

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -97,7 +97,7 @@ export const authenticate = async (req, res, next) => {
   res.cookie("refreshToken", newRefresh, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 2 * 24 * 60 * 60 * 1000,
   });
 
